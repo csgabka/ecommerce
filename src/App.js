ef73daa23d73e1ed13f8d46934a789632304499b
Gabi <csgabka@gmail.com>
2020-09-23 00:13:47 +0100

    sign in

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header';
 import Homepage from './pages/homepage/homepage'
-import ShopPage from './pages/shop/shop'
-
+import ShopPage from './pages/shop/shop';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up';
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path="/" component={Homepage} />
       <Route path="/shop" component={ShopPage} />
+      <Route path="/signin" component={SignInSignUp} />
     </Switch>
     </div>
   );
