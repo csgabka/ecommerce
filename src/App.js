ea110fb1aed664f700f43748c45094353db2737a
Gabi <csgabka@gmail.com>
2020-09-28 14:01:12 +0100

    action types stored in an object and redirecting on sigin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -41,13 +41,20 @@ class App extends React.Component {
   }
 
   render() {
+
+    const { currentUser } = this.props;
     return (
       <div>
       <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInSignUp} />
+        <Route exact path="/signin" render={() => currentUser ? (
+          <Redirect to="/" />
+        ) : (
+          <SignInSignUp />
+        )
+      } />
       </Switch>
       </div>
     );
@@ -55,8 +62,12 @@ class App extends React.Component {
 
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
