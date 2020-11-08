a84ff406b6eba28bc5fdcf0427c9eece3131bd8e
Gabi <csgabka@gmail.com>
2020-11-08 20:18:24 +0000

    hashrouter has been added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
@@ -11,11 +11,11 @@ import {persistor, store } from './redux/store';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   </Provider>,
     document.getElementById('root')
