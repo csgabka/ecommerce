6cc2ce5035c1dcf49a7576757e91225f3301d2f8
Gabi <csgabka@gmail.com>
2020-11-08 20:14:57 +0000

    deployment

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,4 @@
 import { createSelector } from 'reselect';
-import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop;
 
@@ -9,12 +8,12 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
   collections => Object.keys(collections).map(key => collections[key])
 );
 
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections =>
-      collections[collectionUrlParam]
-);
+    collections => collections[collectionUrlParam]
+  );
