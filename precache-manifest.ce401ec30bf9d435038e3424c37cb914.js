1d6106c4c98cea617098c9884db8e5e32625d133
Gabi <csgabka@gmail.com>
2020-11-08 19:58:26 +0000

    Updates

---
+++ precache-manifest.ce401ec30bf9d435038e3424c37cb914.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a57b9f633ac8eac08dc13c65deaed6d",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "5f1c6160ea50fa1730db",
+    "url": "/ecommerce/static/css/main.48989ff6.chunk.css"
+  },
+  {
+    "revision": "518d3410a28f97ecb60f",
+    "url": "/ecommerce/static/js/2.3e014119.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/ecommerce/static/js/2.3e014119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1c6160ea50fa1730db",
+    "url": "/ecommerce/static/js/main.c14c1140.chunk.js"
+  },
+  {
+    "revision": "01d06a9876a12886cdad",
+    "url": "/ecommerce/static/js/runtime-main.9c28059d.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
